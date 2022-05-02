2e1d50ff7cea0282470f28c87b37d2ebb0a60dea
Rachel Kim <rachelminjaekim@gmail.com>
2022-05-02 17:16:12 -0400

    merge intervals

--- Merge Intervals/2-Insert_Interval.js
+++ Merge Intervals/2-Insert_Interval.js
@@ -1,83 +1,31 @@
 function insert(intervals, new_interval) {
   debugger;
   let merged = [];
+  intervals.push(new_interval);
 
-  for (let i = 0; i < intervals.length; i++) {
-    let current = intervals[i];
-    if (new_interval[0] <= current[1]) {
-      // there is overlap
-      current[1] = Math.max(current[1], new_interval[1]);
-      current[0] = Math.min(current[0], new_interval[0]);
-      break;
+  intervals.sort((a, b) => {
+    if (a[0] === b[0]) {
+      return a[1] < b[1] ? -1 : 1;
     } else {
-      merged.push(current);
+      return a[0] < b[0] ? -1 : 1;
+    }
+  });
+
+  let previous = intervals[0];
+  for (let i = 1; i < intervals.length; i++) {
+    let current = intervals[i];
+
+    if (current[0] <= previous[1]) {
+      previous[1] = Math.max(previous[1], current[1]);
+    } else {
+      merged.push(previous);
+      previous = current;
     }
   }
-  merged.push(intervals[i + 1]);
+  merged.push(previous);
   return merged;
 }
-// function insert(intervals, newInterval) {
-//   const result = [];
-
-//   for (let i = 0; i < intervals.length; i++) {
-//     let interval = intervals[i];
-
-//     // If overlaps
-//     if (
-//       Math.max(interval[0], newInterval[0]) <=
-//       Math.min(interval[1], newInterval[1])
-//     ) {
-//       newInterval = [
-//         Math.min(interval[0], newInterval[0]),
-//         Math.max(interval[1], newInterval[1]),
-//       ];
-//       continue;
-//     }
-
-//     // If lower
-//     if (interval[0] > newInterval[1]) {
-//       result.push(newInterval, ...intervals.slice(i));
-//       return result;
-//     }
-
-//     result.push(interval);
-//   }
-
-//   result.push(newInterval);
-//   return result;
-// }
-
-// function insert(intervals, newInterval) {
-//   const result = []
-
-//   /*
-//   Three cases:
-//   1 - If we have already added newInterval or the current interval ends before the new one starts
-//   2 - If newInterval ends before the current one starts
-//   3 - If there is an overlap that requires a merge
-//   */
-
-//   for (const [start, end] of intervals) {
-//       if (!newInterval || end < newInterval[0]) {
-//           result.push([start, end])
-//       } else if (newInterval[1] < start) {
-//           result.push(newInterval)
-//           newInterval = null
-//           result.push([start, end])
-//       } else {
-//           newInterval[0] = Math.min(newInterval[0], start)
-//           newInterval[1] = Math.max(newInterval[1], end)
-//       }
-//   }
-
-//   // If newInterval has not been added it means it must be the last one
-//   if (newInterval) {
-//       result.push(newInterval)
-//   }
-
-//   return result
-// };
-
+// [[1,3], [4,7], [8,12]]
 console.log(
   insert(
     [
@@ -88,3 +36,81 @@ console.log(
     [4, 6]
   )
 );
+//[[1,3], [4,12]]
+console.log(
+  insert(
+    [
+      [1, 3],
+      [5, 7],
+      [8, 12],
+    ],
+    [4, 10]
+  )
+);
+
+class Interval {
+  constructor(start, end) {
+    this.start = start;
+    this.end = end;
+  }
+
+  print_interval() {
+    process.stdout.write(`[${this.start}, ${this.end}]`);
+  }
+}
+
+function insert(intervals, new_interval) {
+  let merged = [],
+    i = 0;
+
+  // skip and add to output) all intervals that come before the 'new_interval'
+  while (i < intervals.length && intervals[i].end < new_interval.start) {
+    merged.push(intervals[i]);
+    i += 1;
+  }
+
+  // merge all intervals that overlap with 'new_interval'
+  while (i < intervals.length && intervals[i].start <= new_interval.end) {
+    new_interval.start = Math.min(intervals[i].start, new_interval.start);
+    new_interval.end = Math.max(intervals[i].end, new_interval.end);
+    i += 1;
+  }
+
+  // insert the new_interval
+  merged.push(new_interval);
+
+  // add all the remaining intervals to the output
+  while (i < intervals.length) {
+    merged.push(intervals[i]);
+    i += 1;
+  }
+
+  return merged;
+}
+
+process.stdout.write("Intervals after inserting the new interval: ");
+let result = insert(
+  [new Interval(1, 3), new Interval(5, 7), new Interval(8, 12)],
+  new Interval(4, 6)
+);
+for (i = 0; i < result.length; i++) {
+  result[i].print_interval();
+}
+console.log();
+
+process.stdout.write("Intervals after inserting the new interval: ");
+result = insert(
+  [new Interval(1, 3), new Interval(5, 7), new Interval(8, 12)],
+  new Interval(4, 10)
+);
+for (i = 0; i < result.length; i++) {
+  result[i].print_interval();
+}
+console.log();
+
+process.stdout.write("Intervals after inserting the new interval: ");
+result = insert([new Interval(2, 3), new Interval(5, 7)], new Interval(1, 4));
+for (i = 0; i < result.length; i++) {
+  result[i].print_interval();
+}
+console.log();
